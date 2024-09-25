c5ad709674b7cf5a2aecf28cfefebf3f770bbaac
riddheshpadma <riddheshpadma.dev@gmail.com>
2024-09-25 14:26:54 +0530

    Login Page Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Dashboard } from './pages'
+import { Dashboard, Login } from './pages'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>  
-    <Dashboard />
+  <StrictMode>
+    {/* <Dashboard /> */}
+    <Login />
   </StrictMode>,
 )
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import loginBg from '../../assets/loginBg.jpg'
 
-const index = () => {
+const Index = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>lodin</div>
-  )
+    <div className='relative flex justify-center items-center h-screen bg-cover bg-center'
+      style={{ backgroundImage: `url(${loginBg})` }}>
+      {/* Apply shadow to the background */}
+      <div className="absolute inset-0 bg-black opacity-75"></div>
+
+      <div className='flex-1 text-right p-10 z-10'>
+        <h1 className='text-5xl font-bold mb-4 text-white'>Welcome to DMS</h1>
+        <p className='pl-10 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, odit et. Modi tempore fugit, temporibus blanditiis explicabo similique! Sapiente at aliquam perspiciatis pariatur dolore.</p>
+      </div>
+
+      <div className='flex-1 justify-center items-center z-10'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div
+              className="relative mx-4 -mt-6 mb-4 grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-primary-color to-secondary-color bg-clip-border text-white shadow-lg shadow-cyan-500/40"
+            >
+              <h3
+                className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased"
+              >
+                Sign In
+              </h3>
+            </div>
+            <div className="flex flex-col gap-4 p-6">
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input
+                  placeholder=""
+                  {...register("email", { required: "Email is required" })}
+                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                />
+                <label
+                  className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+                >
+                  Email
+                </label>
+                {errors.email && <p className="text-red-500 text-xs">{errors.email.message}</p>}
+              </div>
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input
+                  placeholder=""
+                  type="password"
+                  {...register("password", { required: "Password is required" })}
+                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                />
+                <label
+                  className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+                >
+                  Password
+                </label>
+                {errors.password && <p className="text-red-500 text-xs">{errors.password.message}</p>}
+              </div>
+            </div>
+            <div className="p-6 pt-0">
+              <button
+                type="submit"
+                className="block w-full select-none rounded-lg bg-gradient-to-tr from-primary-color to-secondary-color py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-cyan-500/20 transition-all hover:shadow-lg hover:shadow-cyan-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                Sign In
+              </button>
+              <p
+                className="mt-6 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased"
+              >
+                Don't have an account?
+                <a
+                  className="ml-1 block font-sans text-sm font-bold leading-normal text-cyan-500 antialiased"
+                  href="#signup"
+                >
+                  Contact HOD
+                </a>
+              </p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default index
+export default Index;
